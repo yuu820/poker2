acb9053d922bcb7bf8a47d2e137ce2f69dec1ada
yuu820 <yuupc820@gmail.com>
2025-07-22 11:56:26 +0900

    Create package.js

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+  "name": "poker-server",
+  "version": "1.0.0",
+  "description": "Texas Hold'em Poker Server with Socket.IO",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "keywords": ["poker", "websocket", "socket.io", "nodejs"],
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2",
+    "cors": "^2.8.5"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
